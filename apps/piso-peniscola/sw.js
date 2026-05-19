fd5230af7e6e0415e201a9d3392a94eacb1638e5
angelTomo9 <144371630+angelTomo9@users.noreply.github.com>
2026-05-19 17:36:49 +0200

    Final

---
+++ apps/piso-peniscola/sw.js
@@ -0,0 +1,41 @@
+﻿const CACHE_NAME = 'piso-compartido-v1'
+const APP_SHELL = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png']
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .catch(() => undefined),
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((key) => (key === CACHE_NAME ? undefined : caches.delete(key))))),
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+
+  const requestUrl = new URL(event.request.url)
+  if (requestUrl.origin !== self.location.origin) return
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          if (networkResponse && networkResponse.ok) {
+            const responseCopy = networkResponse.clone()
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseCopy))
+          }
+          return networkResponse
+        })
+        .catch(() => caches.match('/index.html'))
+    }),
+  )
+})
