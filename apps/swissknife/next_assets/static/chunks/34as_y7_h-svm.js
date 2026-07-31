14d46ff77d5f67470fc81aef85692d61c6afff9a
angelTomo9 <144371630+angelTomo9@users.noreply.github.com>
2026-07-31 14:36:05 +0200

    feat: mega update with all tools working locally via ToolWorkspace

---
+++ apps/swissknife/next_assets/static/chunks/34as_y7_h-svm.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,46608,t=>{t.v(c=>Promise.all(["static/chunks/3cwxu95ag6kfu.js"].map(c=>t.l(c))).then(()=>c(92542)))}]);
